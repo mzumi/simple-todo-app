a941ee8e9128b079472101a707e024a626dfdaba
mzumi <masazumi.m@gmail.com>
2017-10-28 10:07:21 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  }
+};
